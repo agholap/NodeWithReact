0e05ba7a2877c4911f923641eed0d5853aa2fa34
Amol Gholap <amol.gholap@neudesic.com>
2017-11-28 21:25:00 -0800

    fixed prod variable issues

--- config/keys.js
+++ config/keys.js
@@ -1,16 +1,22 @@
-// module.exports = {
-//   googleClientID:"610220090243-3crumaorn7medjf2stidhllmmm3ib171.apps.googleusercontent.com",
-//   googleClientSecret: "qd4uDCAoEUvyYbQrhMkcDajT",
-//   mongoURI: "mongodb://amol:password@ds011268.mlab.com:11268/emaily1-dev",
-//   cookieKey: "2sadad13131akkkskkskks"
-// };
-
 //key.js - figure out what set of credentials to return
-if(process.env.NODE_ENV === 'production')
-{
-  module.exports = require('./prod');
-}
-else
-{
- module.exports = require('./dev');
+
+//module.exports = require('./dev.js');
+if (process.env.NODE_ENV === "production") {
+  module.exports = {
+    googleClientID:process.env.GOOGLE_CLIENT_ID,     
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY
+  };
+  console.log(process.env);
+} else {
+  //module.exports = require('./dev');
+  module.exports = {
+    googleClientID:
+      "610220090243-3crumaorn7medjf2stidhllmmm3ib171.apps.googleusercontent.com",
+    googleClientSecret: "qd4uDCAoEUvyYbQrhMkcDajT",
+    mongoURI: "mongodb://amol:password@ds011268.mlab.com:11268/emaily1-dev",
+    cookieKey: "2sadad13131akkkskkskks"
+  };  
+
 }
