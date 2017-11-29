a8f37ae164239537995ccdfa3c332a1277de1d4f
Amol Gholap <amol.gholap@neudesic.com>
2017-11-28 16:36:23 -0800

    add new git dir

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,6 @@
 //production key here
 //Dev.js - don't comment
+//need to check the file
 module.exports = {
     googleClientID:process.env.GOOGLE_CLIENT_ID,     
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
